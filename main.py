b909494ed82aa352fa3535e2fc0b5d46513c8753
Thiago <thiagosoteroprado@gmail.com>
2023-09-28 15:02:04 +0000

    Rodando 8 as 20

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import pyautogui
 import time
 
-time.sleep(1)
+# Define o horário de início e término
+hora_inicio = 7  # 4 da manhã
+hora_termino = 23  # termina as 20 da noite
 
 while True:
-    try:
-        if   pyautogui.locateOnScreen('stake.png', confidence=0.8):
-            qdd = pyautogui.locateCenterOnScreen( 'stake.png', confidence=0.8)
+    time.sleep(2)  # Verifica a cada minuto
+    # Obtém a hora atual
+    hora_atual = time.localtime().tm_hour
 
-            pyautogui.click(qdd.x,qdd.y)
+    # Verifica se está dentro do intervalo de tempo desejado
+    if hora_inicio <= hora_atual < hora_termino:
+        try:
+            img = '/home/ubuntu/PycharmProjects/stakeclica/stake.PNG'
 
-    except:
-        pass
+            if pyautogui.locateOnScreen(img, confidence=0.8):
+                qdd = pyautogui.locateCenterOnScreen(img, confidence=0.8)
+                pyautogui.click(qdd.x, qdd.y)
+
+        except Exception as e:
+            print(f"Erro: {e}")
+
+    # Aguarda um período de tempo antes de verificar novamente (ajuste conforme necessário)
+
