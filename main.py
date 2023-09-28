7725af0c5fafb088dbceec8c5dea698d2b47834c
Thiago <thiagosoteroprado@gmail.com>
2023-09-28 22:43:47 +0000

    ajuste aws linux gui

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import time
 from utils import buscaOperacao
 
 # Define o horário de início e término e constantes
-#img = '/home/ubuntu/PycharmProjects/stakeclica/src/stake.PNG'
-img = 'src/tests/imgBtnEnable.png'
+img = '/home/ubuntu/PycharmProjects/stakeclica/src/stake.png'
+#img = 'src/tests/imgBtnEnable.png'
 hora_inicio = 7  # 4 da manhã
 hora_termino = 23  # termina as 20 da noite
 
 while True:
-    time.sleep(2)  # Verifica a cada minuto
+    time.sleep(2)  # Verifica a cada 2 segundos
     # Obtém a hora atual
     hora_atual = time.localtime().tm_hour
 
@@ -27,4 +27,6 @@ while True:
             print(f"Erro: {e}")
 
     # Aguarda um período de tempo antes de verificar novamente (ajuste conforme necessário)
+
+
 
--- utils.py
+++ utils.py
@@ -5,14 +5,16 @@ import os
 
 
 def buscaOperacao():
-    #imgCheck = 'LINUX DIRECTORY/src/test/imgBtnEnable.png'
-    imgCheck = 'src/tests/imgBtnEnable.png'
+    imgCheck = '/home/ubuntu/PycharmProjects/stakeclica/src/botao2.png'
+    # imgCheck = 'src/tests/imgBtnEnable.png'
     sizeScreen = (pyautogui.size().width / 2)
-    valueLeft = '50'
-    valueRight = '200'
+    valueLeft = '0.01'
+    valueRight = '0.03'
 
+    # while True:
+    # time.sleep(5)
     try:
-        location = list(pyautogui.locateAllOnScreen(imgCheck, confidence=0.8))
+        location = list(pyautogui.locateAllOnScreen(imgCheck, confidence=0.99))
         if location:
             for i in location:
                 btnPos = float(i.left)
@@ -22,17 +24,27 @@ def buscaOperacao():
                 if btnPos < sizeScreen:
                     alteraValor(i, valueLeft)
                     print("Vai Ajustar o lado Esquerdo")
+            return
         else:
             print("Não Encontrou")
+            return
     except:
         print("Fodeu")
+        return
 
 
 def alteraValor(item, side):
-    pyautogui.click(float(item.left) - 50, float(item.top))
-    time.sleep(1)
-    pyautogui.typewrite(side, interval=0.5)
-    time.sleep(1)
-    pyautogui.press('Enter')
-    time.sleep(1)
-    pyautogui.press('Delete')
+    pyautogui.click(float(item.left), float(item.top))
+    time.sleep(2)
+    pyautogui.hotkey('ctrl', 'a')
+    pyautogui.press('delete')
+    pyautogui.typewrite(side, interval=1)
+    pyautogui.click(float(item.left), float(item.top) -30)
+    return
+# time.sleep(1)
+# pyautogui.press('Enter')
+# return
+# time.sleep(1)
+# pyautogui.press('Delete')
+
+#buscaOperacao()
